48c50e9062a0f49a2970d4965ed4f87f9c598d7e
n2omatt <n2omatt@amazingcow.com>
2018-04-18 12:01:36 -0700

    - Change the file name.

--- acow/CoreString.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//------------------------------------------------------------------------------
-// Export Headers.
-#include "include/CoreString.h"
-#include "include/CoreString_Utils.h"
-
-
-
